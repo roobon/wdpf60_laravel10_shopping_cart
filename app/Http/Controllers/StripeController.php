f3bdb1568af45d9236aad70355cc167257deee22
G M Saiful Alam <59245133+SaifulAlam93@users.noreply.github.com>
2024-12-07 13:14:02 +0600

    07 December class work

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
 
 class StripeController extends Controller
 {
@@ -11,11 +14,29 @@ class StripeController extends Controller
     {
         \Stripe\Stripe::setApiKey(config('stripe.sk'));
  
+        $orderNum = time();
         $productname = $request->get('productDetails');
         $totalprice = $request->get('total');
         $two0 = "00";
         $total = "$totalprice$two0";
- 
+        // Order entry
+        DB::table('orders')->insert([
+            'order_number' => $orderNum,
+            'total_amount' => $totalprice,
+            'payment_method' => 'stripe'
+        ]);
+
+        // Order Details entry
+        foreach(session()->get('cart') as $key => $val){
+            DB::table('order_details')->insert([
+                'order_number' => $orderNum,
+                'book_id' => $key,
+                'quantity' => $val['quantity'],
+                'price' => $val['price'],
+            ]);
+        }
+
+
         $session = \Stripe\Checkout\Session::create([
             'line_items'  => [
                 [
@@ -43,4 +64,12 @@ class StripeController extends Controller
     {
         return view('success');
     }
+
+    // public function productOrder(){
+    //    $cart = session()->get('cart');
+    //     //echo $cart->name;
+    //    $cart[3];
+        
+    //     //DB::table('orders');
+    // }
 }
